d7bf152e37bf4c9ab2082596cefd780081a2bbb3
Richard Ng <richardcrng@gmail.com>
2021-02-17 19:24:03 +0000

    Add note on generics

--- src/utils/pickRandom.ts
+++ src/utils/pickRandom.ts
@@ -1,3 +1,9 @@
+/**
+ * Typing is unimportant here, but - for the interested - it's
+ *  using generic types:
+ *
+ * https://www.typescriptlang.org/docs/handbook/generics.html
+ */
 export function pickRandom<T>(arr: T[]): T {
   const randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
